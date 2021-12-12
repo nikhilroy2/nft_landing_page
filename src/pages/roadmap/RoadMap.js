3fdbdd9d4cc2fb1059b8d75fcdbb686f3d0bd7a2
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2021-12-12 08:44:08 +0600

    roadmap done

--- src/pages/roadmap/RoadMap.js
+++ src/pages/roadmap/RoadMap.js
@@ -6,21 +6,47 @@ function RoadMap(props) {
       <h1>ROADMAP</h1>
       <div className="roadMap_lineWrapper">
         <div className="road_line_1">
-          <div className="line"></div>
-          <div className="line_text">
+          <div className="road_circle">X1</div>
+        </div>
+        <div className="road_line_2">
+          <div className="road_circle">X2</div>
+        </div>
+        <div className="road_line_3">
+          <div className="road_circle">X3</div>
+        </div>
+        <div className="road_line_4">
+          <div className="road_circle">X4</div>
+        </div>
+        <div className="road_line_5">
+          <div className="road_circle">X5</div>
+        </div>
+
+        {/* roadmap content */}
+        <div className="roadmap_content">
+          <div className="content_item_1">
             <h2>LAUNCH</h2>
             <p>First generation Warriors are avaiable to mint and stake</p>
           </div>
-        </div>
-        <div className="road_line_2">
-          <div className="line_text">
+          <div className="content_item_2">
             <h2>ARENA MODE</h2>
             <p>
               Arena mode in which warriors are able to battle each other and put
               SILVER on the line{" "}
             </p>
           </div>
-          <div className="line"></div>
+          <div className="content_item_3">
+            <h2>NEW WARRIORS</h2>
+            <p>
+              NEW warrior types with different and varying abilities and
+              attributes
+            </p>
+          </div>
+          <div className="content_item_4">
+            <h2>TBA</h2>
+          </div>
+          <div className="content_item_5">
+            <h2>TBA</h2>
+          </div>
         </div>
       </div>
     </section>
